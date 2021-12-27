f150a7232b5beb51e8aa27583a47025bd1f2437b
shon-arch <sunnywahane14@gmail.com>
2021-12-27 22:05:42 +0530

    Added a new route to verify the the right coordinate and for the character currently dont do the verification

--- where_is_waldo/routes.py
+++ where_is_waldo/routes.py
@@ -1,6 +1,13 @@
 from where_is_waldo import app 
-from flask import render_template
+from flask import render_template, request
 
 @app.route("/")
 def home():
     return render_template("level.html")
+
+@app.route("/verify/<character>", methods=['GET'])
+def verify(character):
+    x_cordinate = request.args.get('x_cordinate', type=int)
+    y_cordinate = request.args.get('y_cordinate', type=int)
+    
+    return True
