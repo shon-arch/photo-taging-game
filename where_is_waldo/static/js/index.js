9cb53740d3fc8f9a085675f12ef32ab64c4ca9ec
shon-arch <sunnywahane14@gmail.com>
2021-12-30 14:05:14 +0530

    Added the logic that starts the game and timer when the user click the 'start' button on the modal

--- where_is_waldo/static/js/index.js
+++ where_is_waldo/static/js/index.js
@@ -1,34 +1,81 @@
 const game_image = document.getElementsByClassName("root-div")[0]
 
 characters = new Set()
-characters.add("c1")
-characters.add("c2")
-characters.add("c3")
+characters.add("Leo")
+characters.add("Goku")
+characters.add("Mike")
 
-function verify_answer(event){
+all_char = ["Leo", "Goku", "Mike"]
+
+
+function startGame(){
+
+    game_image.style.display = "block"
+    document.getElementById("nav").style.display = "flex"
+    document.getElementById("modal").style.display = "none"
+
+    var countDownDate = new Date().getTime(); 
+
+    var x = setInterval(function(){
+    
+    var now = new Date().getTime(); 
+    var distance = now - countDownDate; 
+    
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById("nav-h1").innerText = minutes + "m:" + seconds + "s ";
+    
+    } ,1000);
+}
+
+
+
+
+for(let x in all_char){
+    img = document.createElement("img")
+    img.classList.add("nav-image")
+    img.src = "/static/images/" + all_char[x] + ".png"
+    if(!characters.has(all_char[x])){
+        img.classList.add("disabled")
+    }
+    document.getElementById("images").appendChild(img)
+}
+
+
+
+
+
+
+
+
+async function verify_answer(name, x_pos, y_pos){
     const existing_drop_down = game_image.getElementsByClassName("drop-down")
     game_image.removeChild(existing_drop_down[0]); 
-    fetch('/verify?'+event.target.innerText).then(function(response){
-        if(response){
-           characters.delete(event.target.innerText)
-        }
-    })
+    response = await fetch('/verify/'+name+'?'+'x_coordinate=' + String(x_pos) +'&y_coordinate=' + String(y_pos));
+    result = await response.json()
+    if(result['Message'] == 'success'){
+           characters.delete(name)
+    }
 }
 
 function generate_drop_down(event){
     const drop_down = document.createElement("ul")
     drop_down.classList.add("drop-down")
 
+    let x_pos = event.layerX
+    let y_pos = event.layerY
+
     for(let item of characters){
         option = document.createElement("li")
         option.innerText=item
-        option.addEventListener("click", verify_answer)
+        option.addEventListener("click", function(event) {verify_answer(event.target.innerText,x_pos,y_pos)})
         drop_down.appendChild(option)
 
     }
 
-    drop_down.style.left = event.layerX + "px"; 
-    drop_down.style.top = event.layerY + "px";
+    drop_down.style.left = x_pos + "px"; 
+    drop_down.style.top = y_pos + "px";
 
     return drop_down
 }
