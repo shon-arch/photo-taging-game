c5668a09df6937dd9ec7335499a563bb8e0e0b75
shon-arch <sunnywahane14@gmail.com>
2021-12-30 14:03:46 +0530

    Added validation for one character

--- where_is_waldo/routes.py
+++ where_is_waldo/routes.py
@@ -1,13 +1,20 @@
-from where_is_waldo import app 
+from where_is_waldo import app, db_object
 from flask import render_template, request
 
 @app.route("/")
 def home():
     return render_template("level.html")
 
-@app.route("/verify/<character>", methods=['GET'])
+@app.route("/verify/<character>")
 def verify(character):
-    x_cordinate = request.args.get('x_cordinate', type=int)
-    y_cordinate = request.args.get('y_cordinate', type=int)
+    x_coordinate = request.args.get('x_coordinate', type=int)
+    y_coordinate = request.args.get('y_coordinate', type=int)
+    character_data = db_object.db.characterCoordinates.find_one_or_404({'name':'Goku'})
     
-    return True
+    if((x_coordinate <= character_data['x_coordinate']+20) and
+       (x_coordinate >=character_data['x_coordinate']-20) and
+       (y_coordinate <= character_data['y_coordinate']+40) and
+       (y_coordinate >=character_data['y_coordinate']-40)):
+        return {'Message' : 'success'}
+    return {'Message' : 'failed'}
+    
