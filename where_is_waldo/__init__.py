00e144199c3c2cc765bcb55d2e8595362ba4eb0b
shon-arch <sunnywahane14@gmail.com>
2022-01-29 12:40:57 +0530

    created a pymonog object to access the database in mongodb atlas

--- where_is_waldo/__init__.py
+++ where_is_waldo/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+import os
+import urllib 
+
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://waldo:" + urllib.parse.quote(os.environ.get('PASSWORD')) + "@cluster0.f496s.mongodb.net/waldo?retryWrites=true&w=majority"
+db_object= PyMongo(app)
+
 
 from where_is_waldo import routes
