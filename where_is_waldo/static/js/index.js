78dcd1c317c7dfaa8d8dab77ed5a0ea1f50dd31e
shon-arch <sunnywahane14@gmail.com>
2022-01-30 19:43:51 +0530

    Added function to show popus when the player wins

--- where_is_waldo/static/js/index.js
+++ where_is_waldo/static/js/index.js
@@ -1,12 +1,45 @@
 const game_image = document.getElementsByClassName("root-div")[0]
 
 characters = new Set()
-characters.add("Leo")
-characters.add("Goku")
-characters.add("Mike")
+all_char = []
 
-all_char = ["Leo", "Goku", "Mike"]
+user_data = {
+    'startTime' : null, 
+    'endTime': null,
+    'level' : null
+}
 
+async function submitScore(){
+
+    const data = {
+        'username' : document.getElementById("user-name").value,
+        'time' : user_data['endTime'] - user_data['startTime'], 
+        'level' : user_data['level']
+    }
+    var head = { "Content-Type" : "application/json" };
+    const options = {
+        method: 'POST', 
+        body: JSON.stringify(data), 
+        headers : head
+    }
+    await fetch('/post_user', options)
+    window.location.href = '/leaderboard'
+}
+
+function populate(characters_list, lvl){
+    user_data.startTime = new Date().getTime(); 
+    user_data.level = lvl; 
+    characters_list.forEach(element => {
+        characters.add(element['name']); 
+        all_char.push(element['name']); 
+
+        img = document.createElement("img")
+        img.classList.add("nav-image")
+        img.classList.add(element['name'])
+        img.src = "/static/images/" + element['name'] + ".png"
+        document.getElementById("images").appendChild(img)
+    })
+}
 
 function startGame(){
 
@@ -14,38 +47,7 @@ function startGame(){
     document.getElementById("nav").style.display = "flex"
     document.getElementById("modal").style.display = "none"
 
-    var countDownDate = new Date().getTime(); 
-
-    var x = setInterval(function(){
-    
-    var now = new Date().getTime(); 
-    var distance = now - countDownDate; 
-    
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    document.getElementById("nav-h1").innerText = minutes + "m:" + seconds + "s ";
-    
-    } ,1000);
 }
-
-
-
-
-for(let x in all_char){
-    img = document.createElement("img")
-    img.classList.add("nav-image")
-    img.src = "/static/images/" + all_char[x] + ".png"
-    if(!characters.has(all_char[x])){
-        img.classList.add("disabled")
-    }
-    document.getElementById("images").appendChild(img)
-}
-
-
-
-
-
 
 
 
@@ -54,9 +56,32 @@ async function verify_answer(name, x_pos, y_pos){
     game_image.removeChild(existing_drop_down[0]); 
     response = await fetch('/verify/'+name+'?'+'x_coordinate=' + String(x_pos) +'&y_coordinate=' + String(y_pos));
     result = await response.json()
+    message='Try again'
+    success = false; 
     if(result['Message'] == 'success'){
+            message = 'Yay! You found ' + name; 
+            success= true; 
+           document.getElementsByClassName(name)[0].classList.add("disabled")
            characters.delete(name)
     }
+    await show_pop_up(message, success); 
+    if(characters.size == 0){
+        game_image.style.display = "none"
+        document.getElementById("nav").style.display = "none"
+        document.getElementById("user-modal").style.display = "block"
+    }
+}
+
+function show_pop_up(message, success){
+    const span = document.getElementById("status")
+    span.textContent = message; 
+    if(success)span.style.backgroundColor = "green"; 
+    else span.style.backgroundColor = "red"; 
+    span.style.marginTop = document.getElementById("nav").style.height; 
+    setTimeout(function(){
+        span.innerHTML=''; 
+    },1500)
+
 }
 
 function generate_drop_down(event){
@@ -66,10 +91,16 @@ function generate_drop_down(event){
     let x_pos = event.layerX
     let y_pos = event.layerY
 
+    let x_ratio = x_pos/event.target.width
+    let y_ratio = (y_pos-140)/event.target.height
+
     for(let item of characters){
         option = document.createElement("li")
         option.innerText=item
-        option.addEventListener("click", function(event) {verify_answer(event.target.innerText,x_pos,y_pos)})
+        option.addEventListener("click", function(event) {
+            user_data.endTime = new Date().getTime();  
+            verify_answer(event.target.innerText,x_ratio,y_ratio)
+        })
         drop_down.appendChild(option)
 
     }
@@ -82,8 +113,7 @@ function generate_drop_down(event){
 
 
 game_image.addEventListener("click", function (event){
-        if(event.target != document.getElementById("game_image"))return
-        k=event
+        if(event.target != document.getElementById("game_image"))return 
         const existing_drop_down = game_image.getElementsByClassName("drop-down")
         if(existing_drop_down.length !=0){
             game_image.removeChild(existing_drop_down[0]); 
